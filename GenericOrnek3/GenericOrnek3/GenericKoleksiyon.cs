f2487e4c9790a5aa12498e709373d0728622afe9
Buğra AYDIN <31183177+bugra95@users.noreply.github.com>
2022-05-19 01:02:14 +0300

    GenericExample2
    a simple example and advantages for generic

---
+++ GenericOrnek3/GenericOrnek3/GenericKoleksiyon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericOrnek3
+{
+    class GenericKoleksiyon<T>
+    {
+        T[] dizi;
+
+        public GenericKoleksiyon()
+        {
+            dizi = new T[4];
+        }
+
+        public void ElemanEkle(T eleman)
+        {
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if(dizi[i] == null)
+                {
+                    dizi[i] = eleman;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ GenericOrnek3/GenericOrnek3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericOrnek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericKoleksiyon<string> koleksiyon = new GenericKoleksiyon<string>();
+
+            koleksiyon.ElemanEkle("Ankara");
+            koleksiyon.ElemanEkle("İstanbul");
+            koleksiyon.ElemanEkle("İzmir");
+            koleksiyon.ElemanEkle("Eskişehir");
+
+            Console.ReadLine();
+        }
+    }
+}
